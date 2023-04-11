8fb8c4397f21bfcacb12ffbefb8fa956248b5ec9
Henrik Bakken <bakken.henrik@gmail.com>
2023-04-11 12:56:37 +0200

    fish

--- fish/config.fish
+++ fish/config.fish
@@ -6,8 +6,3 @@ set -x PAGER "less -isgj.5"
 set -x CUDA_HOME /opt/cuda
 set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH $CUDA_HOME/lib64
 set -Ua fish_user_paths /home/hjalmarlucius/.local/bin/
-
-if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
-end
