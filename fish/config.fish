19ea3f495c47dba90dce8042a06d4f171a537fef
Henrik Bakken <bakken.henrik@gmail.com>
2023-07-19 03:24:15 +0200

    cleanup on mothership reinstall

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,7 @@
-pyenv init - | source
-set -x QT_QPA_PLATFORMTHEME "qt5ct"
-set -x GTK2_RC_FILES "$HOME/.gtkrc-2.0"
-set -x PAGER "less -isgj.5"
-
-# cuda stuff
-set -x CUDA_HOME /opt/cuda
-set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH $CUDA_HOME/lib64
-set -Ua fish_user_paths /home/hjalmarlucius/.local/bin/
+if status is-interactive
+    pyenv init - | source
+    set -x PAGER "less -isgj.5"
+    set -Ua fish_user_paths /home/hjalmarlucius/.local/bin/
+    set -x CUDA_HOME /opt/cuda
+    set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH $CUDA_HOME/lib64
+end
