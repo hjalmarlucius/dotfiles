802c785a53a716c73ab7676cb22401a294a32761
Henrik Bakken <bakken.henrik@gmail.com>
2025-02-14 17:41:42 +0100

    nvim tmux navigator change plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -242,25 +242,18 @@ require("lazy").setup({
         },
         {
             -- tmux / vim interop
-            "christoomey/vim-tmux-navigator",
-            cmd = {
-                "TmuxNavigateLeft",
-                "TmuxNavigateDown",
-                "TmuxNavigateUp",
-                "TmuxNavigateRight",
-                "TmuxNavigatePrevious",
-                "TmuxNavigatorProcessList",
-            },
-            keys = {
-                { "<M-h>", "<cmd>TmuxNavigateLeft<cr>" },
-                { "<M-j>", "<cmd>TmuxNavigateDown<cr>" },
-                { "<M-k>", "<cmd>TmuxNavigateUp<cr>" },
-                { "<M-l>", "<cmd>TmuxNavigateRight<cr>" },
-                { "<M-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
-            },
-            init = function()
-                vim.g.tmux_navigator_no_mappings = 1
-                vim.g.tmux_navigator_disable_when_zoomed = 1
+            "alexghergh/nvim-tmux-navigation",
+            config = function()
+                require("nvim-tmux-navigation").setup({
+                    disable_when_zoomed = true, -- defaults to false
+                    keybindings = {
+                        left = "<M-h>",
+                        down = "<M-j>",
+                        up = "<M-k>",
+                        right = "<M-l>",
+                        last_active = "<M-\\>",
+                    },
+                })
             end,
         },
         {
