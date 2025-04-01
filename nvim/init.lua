9d8f7eed098e817c4e2aed8f7b469a6ba140771a
Henrik Bakken <bakken.henrik@gmail.com>
2025-04-02 01:24:27 +0200

    nvim render-markdown added

--- nvim/init.lua
+++ nvim/init.lua
@@ -674,6 +674,31 @@ local function makespecs_previewers()
                 vim.g.mkdp_preview_options = { disable_sync_scroll = 1 }
             end,
         },
+        {
+            "MeanderingProgrammer/render-markdown.nvim",
+            dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+            opts = {
+                code = { sign = false, width = "block", right_pad = 1 },
+                header = { sign = false, icons = {} },
+                checkbox = { enabled = false },
+            },
+            ft = { "markdown" },
+            config = function(_, opts)
+                require("render-markdown").setup(opts)
+                Snacks.toggle({
+                    name = "Render Markdown",
+                    get = function() return require("render-markdown.state").enabled end,
+                    set = function(enabled)
+                        local m = require("render-markdown")
+                        if enabled then
+                            m.enable()
+                        else
+                            m.disable()
+                        end
+                    end,
+                }):map("<leader>um")
+            end,
+        },
     }
 end
 
